44472569202a7fab23bc261b5b5a2be89d9b1582
natdjerf <nadjerf@gmail.com>
2016-04-05 14:52:55 -0400

    Add sign-in and sign-up functions.
    Successfully returned token / user ID.

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,25 +1,26 @@
 'use strict';
 
 const signUp = (success, failure, data) => {
-    Math.random() > 0.5 ? success('in signUp') : failure(data);
+  $.ajax({
+    method: 'POST',
+    url: 'http://tic-tac-toe.wdibos.com/sign-up',
+    data,
+  }).done(success)
+  .fail(failure);
+};
+
+const signIn = (success, failure, data) => {
+  $.ajax({
+    method: 'POST',
+    url: 'http://tic-tac-toe.wdibos.com/sign-in',
+    data,
+  }).done(success)
+  .fail(failure);
   };
-
-
-
-
-
-//   $.ajax({
-//     method: 'POST',
-//     url: 'http://tic-tac-toe.wdibos.com/sign-up',
-//     data,
-//   }).done(success)
-//   .fail(failure);
-// };
-
-
 
 
 
 module.exports = {
   signUp,
+  signIn,
 };
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -5,13 +5,21 @@ const authUi = require('./ui');
 const getFormFields = require('../../../lib/get-form-fields');
 
 const addHandlers = () => {
+  $('#sign-up').on('submit', function (event) {
+    event.preventDefault();
+    let data = getFormFields(this);
+    console.log('sign up has been clicked.');
+    authApi.signUp(authUi.success, authUi.failure, data);
+  });
   $('#sign-in').on('submit', function (event) {
-  event.preventDefault();
-  let data = getFormFields(this);
-  console.log('Sign in clicked');
-  authApi.signUp(authUi.success, authUi.failure, data);
+    event.preventDefault();
+    let data = getFormFields(this);
+    console.log('Sign in clicked');
+    authApi.signIn(authUi.success, authUi.failure, data);
+    console.log(data);
   });
 };
+
 
 // $('#sign-in').find("[name='credentials[email]']")
 //$('#sign-up').find("[name='credentials[email]']").val();
